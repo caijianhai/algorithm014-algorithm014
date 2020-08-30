08a3291e88744c21c94111f690498937be6381e4
蔡建海 <caijianhai@staff.hexun.com>
2020-08-30 22:39:35 +0800

    week3

--- Week_03/组合.swift
+++ Week_03/组合.swift
@@ -1,22 +1,4 @@
-/*
- 
- 77. 组合
- 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
 
- 示例:
-
- 输入: n = 4, k = 2
- 输出:
- [
-   [2,4],
-   [3,4],
-   [2,3],
-   [1,2],
-   [1,3],
-   [1,4],
- ]
-
- */
 
 class Solution {
     
@@ -58,6 +40,3 @@ class Solution {
         bine(&result, &subSets, num: num + 1, count: count, k)
     }
 }
-
-
-print(Solution().combine(3, 2))
